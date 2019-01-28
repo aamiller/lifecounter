8102325f52f8665d594a9a113396ab9f67eb1999
aamiller <adelem@uw.edu>
2019-01-27 23:46:03 -0800

    Layout with some clipping

---
+++ lifecounter/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  lifecounter
+//
+//  Created by Adele on 1/24/19.
+//  Copyright © 2019 edu.washington. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func player_one_increase(_ sender: Any) {
+    }
+    
+}
+
