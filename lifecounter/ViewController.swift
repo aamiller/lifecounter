b4db7742567317ea2ac5b78c145fc3e54f5d8662
aamiller <adelem@uw.edu>
2019-01-28 16:59:11 -0800

    Finished code

--- lifecounter/ViewController.swift
+++ lifecounter/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var OutcomeLabel: UILabel!
     @IBOutlet weak var PlayerOneScore: UILabel!
+    @IBOutlet weak var PlayerTwoScore: UILabel!
+    @IBOutlet weak var PlayerThreeScore: UILabel!
+    @IBOutlet weak var PlayerFourScore: UILabel!
     var losers : [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -30,13 +34,62 @@ class ViewController: UIViewController {
         PlayerOneScore.text = newScore(PlayerOneScore.text, -1, "1")
     }
     
+    
+    @IBAction func PlayerTwo_MinusOne(_ sender: Any) {
+        PlayerTwoScore.text = newScore(PlayerTwoScore.text, -1, "2")
+    }
+    @IBAction func PlayerTwo_PlusOne(_ sender: Any) {
+        PlayerTwoScore.text = newScore(PlayerTwoScore.text, 1, "2")
+    }
+    @IBAction func PlayerTwo_MinusFive(_ sender: Any) {
+        PlayerTwoScore.text = newScore(PlayerTwoScore.text, -5, "2")
+    }
+    
+    @IBAction func PlayerTwo_PlusFive(_ sender: Any) {
+        PlayerTwoScore.text = newScore(PlayerTwoScore.text, 5, "2")
+    }
+    
+    
+    @IBAction func PlayerThree_MinusOne(_ sender: Any) {
+        PlayerThreeScore.text = newScore(PlayerThreeScore.text, -1, "3")
+    }
+    
+    @IBAction func PlayerThree_PlusOne(_ sender: Any) {
+        PlayerThreeScore.text = newScore(PlayerThreeScore.text, 1, "3")
+    }
+    
+    @IBAction func PlayerThree_MinusFive(_ sender: Any) {
+        PlayerThreeScore.text = newScore(PlayerThreeScore.text, -5, "3")
+    }
+    @IBAction func PlayerThree_PlusFive(_ sender: Any) {
+        PlayerThreeScore.text = newScore(PlayerThreeScore.text, 5, "3")
+    }
+    
+    @IBAction func PlayerFour_MinusOne(_ sender: Any) {
+        PlayerFourScore.text = newScore(PlayerFourScore.text, -1, "4")
+        
+    }
+    @IBAction func PlayerFour_PlusOne(_ sender: Any) {
+        PlayerFourScore.text = newScore(PlayerFourScore.text, 1, "4")
+    }
+    @IBAction func PlayerFour_MinusFive(_ sender: Any) {
+        PlayerFourScore.text = newScore(PlayerFourScore.text, -5, "4")
+    }
+    
+    @IBAction func PlayerFour_PlusFive(_ sender: Any) {
+        PlayerFourScore.text = newScore(PlayerFourScore.text, 5, "4")
+    }
+    
     func newScore(_ oldScore: String?, _ scoreChange: Int, _ player: String) -> String {
-        let currScore : String = PlayerOneScore.text ?? "0"
+        let currScore : String = oldScore ?? "0"
         let newScore = Int(currScore)! + scoreChange
         if newScore <= 0 {
             if !losers.contains(player) {
                 losers.append(player)
                 OutcomeLabel.text = OutcomeLabel.text! +  " Player " + player + " LOSES!"
+                if losers.count == 2 {
+                    OutcomeLabel.text = OutcomeLabel.text! + "\n"
+                }
             }
         }
         return String(newScore)
