2f2ecafbd22cfff30ee703a43bc2f6f17e2e2f02
aamiller <adelem@uw.edu>
2019-01-30 08:45:39 -0800

    Score adjusting buttons work for all 8 players

--- lifecounter/ViewController.swift
+++ lifecounter/ViewController.swift
@@ -28,21 +28,45 @@ class ViewController: UIViewController {
     
     @IBOutlet var PlayerEightButtons: [UIButton]!
     
+    var increment_vals = [-1, 1, -5, 5]
+    
+    var losers: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    func newScore(_ oldScore: String?, _ scoreChange: Int, _ player: String) -> String {
+    @IBAction func ChangeValueButtonPressed(_ sender: UIButton) {
+        
+        if PlayerOneButtons.contains(sender) {
+            PlayerScore[0].text = newScore(PlayerScore[0].text, sender.tag, "1")
+        } else if PlayerTwoButtons.contains(sender) {
+            PlayerScore[1].text = newScore(PlayerScore[1].text, sender.tag, "2")
+        } else if PlayerThreeButtons.contains(sender) {
+            PlayerScore[2].text = newScore(PlayerScore[2].text, sender.tag, "3")
+        } else if PlayerFourButtons.contains(sender) {
+            PlayerScore[3].text = newScore(PlayerScore[3].text, sender.tag, "4")
+        } else if PlayerFiveButtons.contains(sender) {
+            PlayerScore[4].text = newScore(PlayerScore[4].text, sender.tag, "5")
+        } else if PlayerSixButtons.contains(sender) {
+            PlayerScore[5].text = newScore(PlayerScore[5].text, sender.tag, "6")
+        } else if PlayerSevenButtons.contains(sender) {
+            PlayerScore[6].text = newScore(PlayerScore[6].text, sender.tag, "7")
+        } else if PlayerEightButtons.contains(sender) {
+            PlayerScore[7].text = newScore(PlayerScore[7].text, sender.tag, "8")
+        }
+    }
+    
+
+
+    func newScore(_ oldScore: String?, _ scoreChangeTag: Int, _ player: String) -> String {
         let currScore : String = oldScore ?? "0"
-        let newScore = Int(currScore)! + scoreChange
+        let newScore = Int(currScore)! + increment_vals[scoreChangeTag - 1]
+        
         if newScore <= 0 {
             if !losers.contains(player) {
                 losers.append(player)
-                OutcomeLabel.text = OutcomeLabel.text! +  " Player " + player + " LOSES!"
-                if losers.count == 2 {
-                    OutcomeLabel.text = OutcomeLabel.text! + "\n"
-                }
             }
         }
         return String(newScore)
