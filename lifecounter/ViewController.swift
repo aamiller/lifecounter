46dba78408fd8b415d5eafb8c08325734633d32f
aamiller <adelem@uw.edu>
2019-01-30 13:02:30 -0800

    Finish enable/disable for remove/add player buttons

--- lifecounter/ViewController.swift
+++ lifecounter/ViewController.swift
@@ -30,25 +30,50 @@ class ViewController: UIViewController {
     
     @IBOutlet var PlayerEightButtons: [UIButton]!
     
-    var increment_vals = [-1, 1, -5, 5]
+    @IBOutlet var Add_Remove_Player_Buttons: [UIButton]!
+    
+    // Button press values for all players
+    var increment_vals = [[-1, 1, -5, 5], [-1, 1, -5, 5],
+                          [-1, 1, -5, 5], [-1, 1, -5, 5],
+                          [-1, 1, -5, 5], [-1, 1, -5, 5],
+                          [-1, 1, -5, 5], [-1, 1, -5, 5],
+                          [-1, 1, -5, 5], [-1, 1, -5, 5]]
     
     var losers: [String] = []
     
-    var player_count = 2
+    var gameHistory: String = "Game begins. "
+    
+    var player_count = 4
+    
+    var gameStarted : Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBAction func AddPlayerButtonPress(_ sender: Any) {
-        if player_count < 8 {
-            PlayerStackViews[player_count].isHidden = false
-            player_count += 1
+    @IBAction func UpdatePlayersButtonPress(_ sender: UIButton) {
+        if !gameStarted {
+            switch sender.tag {
+            case 1: // Remove player
+                if player_count > 2 {
+                    PlayerStackViews[player_count - 1].isHidden = true
+                    player_count -= 1
+                }
+            case 2: // Add player
+                if player_count < 8 {
+                    PlayerStackViews[player_count].isHidden = false
+                    player_count += 1
+                }
+            default: break
+            }
         }
     }
-    
+
     @IBAction func ChangeValueButtonPressed(_ sender: UIButton) {
+        gameStarted = true
+        Add_Remove_Player_Buttons[0].isEnabled = false
+        Add_Remove_Player_Buttons[1].isEnabled = false
         
         if PlayerOneButtons.contains(sender) {
             PlayerScore[0].text = newScore(PlayerScore[0].text, sender.tag, "1")
@@ -70,14 +95,26 @@ class ViewController: UIViewController {
     }
     
 
-
     func newScore(_ oldScore: String?, _ scoreChangeTag: Int, _ player: String) -> String {
         let currScore : String = oldScore ?? "0"
-        let newScore = Int(currScore)! + increment_vals[scoreChangeTag - 1]
-        
+        let newScore = Int(currScore)! + increment_vals[Int(player)! - 1][scoreChangeTag - 1]
+        let playerDetails : String = "Player " + player
+        switch scoreChangeTag {
+            case 1: gameHistory = gameHistory + playerDetails + " lost 1 life."
+            case 2: gameHistory = gameHistory + "Player " + player + " gained 1 life."
+            case 3:
+                let scoreChange : Int = abs(increment_vals[Int(player)! - 1][scoreChangeTag - 1])
+                gameHistory = gameHistory + String(playerDetails) + " lost "  + String(scoreChange) + " life."
+            case 4:
+                let scoreChange : Int = increment_vals[Int(player)! - 1][scoreChangeTag - 1]
+                gameHistory = gameHistory + String(playerDetails) + " gained " + String(scoreChange) + " life."
+        default: break
+        }
+
         if newScore <= 0 {
             if !losers.contains(player) {
                 losers.append(player)
+                gameHistory = gameHistory + "Player " + player + " lost. "
             }
         }
         return String(newScore)
